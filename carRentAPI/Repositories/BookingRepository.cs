7ef966954613efdfbd3b5b67e33211f2a3b6da78
Dilmi <dilmithisara343@gmail.com>
2023-12-05 22:09:31 +0530

    write booking repository

--- carRentAPI/Repositories/BookingRepository.cs
+++ carRentAPI/Repositories/BookingRepository.cs
@@ -4,6 +4,7 @@ using MongoDB.Driver;
 
 namespace carRentAPI.Repositories
 {
+    //booking repository
 	public class BookingRepository
 	{
         private readonly IMongoCollection<Booking> _bookingsCollection;
@@ -12,16 +13,20 @@ namespace carRentAPI.Repositories
         {
             _bookingsCollection = database.GetCollection<Booking>("bookings");
         }
-
+        
+        //get booking detail repository
         public IEnumerable<Booking> GetBookings() => _bookingsCollection.Find(booking => true).ToList();
 
+        //get booking detail by ID
         public Booking GetBooking(string id) => _bookingsCollection.Find(booking => booking.Id == id).FirstOrDefault();
 
+        //save booking detail
         public void AddBooking(Booking booking) => _bookingsCollection.InsertOne(booking);
 
+        // update booking detail
         public void UpdateBooking(string id, Booking updatedBooking) =>
             _bookingsCollection.ReplaceOne(booking => booking.Id == id, updatedBooking);
-
+        // delete booking detail
         public void DeleteBooking(string id) =>
             _bookingsCollection.DeleteOne(booking => booking.Id == id);
     }
