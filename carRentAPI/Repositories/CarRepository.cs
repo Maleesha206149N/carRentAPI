83f8105f643ad66fb5e70be5a878a27aa8ad33bc
pansila <hansaniliyanage17@gmail.com>
2023-12-05 22:21:37 +0530

    write car repository

--- carRentAPI/Repositories/CarRepository.cs
+++ carRentAPI/Repositories/CarRepository.cs
@@ -5,6 +5,7 @@ using carRentAPI.Models;
 
 namespace carRentAPI.Repositories
 {
+    //car repository
     public class CarRepository
 	{
         private readonly IMongoCollection<Vehicle> _carsCollection;
@@ -14,15 +15,20 @@ namespace carRentAPI.Repositories
             _carsCollection = database.GetCollection<Vehicle>("cars");
         }
 
+        // car details get repository
         public IEnumerable<Vehicle> GetCars() => _carsCollection.Find(car => true).ToList();
 
+        // car deatails get by ID
         public Vehicle GetCar(string id) => _carsCollection.Find(car => car.Id == id).FirstOrDefault();
 
+        //save car detail
         public void AddCar(Vehicle car) => _carsCollection.InsertOne(car);
 
+        //update car detail
         public void UpdateCar(string id, Vehicle updatedCar) =>
             _carsCollection.ReplaceOne(car => car.Id == id, updatedCar);
 
+        // delete car detail
         public void DeleteCar(string id) =>
             _carsCollection.DeleteOne(car => car.Id == id);
     }
